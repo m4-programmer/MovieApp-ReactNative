ceaadcba63a9640ad2670ba38e57fb833dc583ca
Adolphus Miracle <miraboy13@gmail.com>
2024-02-03 12:39:28 +0100

    refactor: added logic to navigate to search screen from home screen

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -5,6 +5,7 @@ import { Bars3CenterLeftIcon, MagnifyingGlassIcon} from 'react-native-heroicons/
 import { styles as themes } from './../themes'
 import TrendingMovies from '../components/TrendingMovies';
 import MovieList from '../components/MovieList';
+import { useNavigation } from '@react-navigation/native';
 
 
 const  ios =  Platform.OS == 'ios'
@@ -13,6 +14,7 @@ const HomeScreen = () => {
   const [trending, setTrending] = React.useState([1,2,3])
   const [upcoming, setUpcoming] = React.useState([1,2,3])
   const [topRated, setTopRated] = React.useState([1,2,3])
+  const navigation = useNavigation();
   return (
     <View className='flex-1 bg-neutral-800'>
         {/* search bar and logo */}
@@ -23,7 +25,7 @@ const HomeScreen = () => {
             <Text className="text-white text-3xl font-bold">
                 <Text style={themes.text} >M</Text>ovies
             </Text>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate('Search')}>
                 <MagnifyingGlassIcon size={30} strokeWidth={2} color={"white"} />
             </TouchableOpacity>
         </View>
